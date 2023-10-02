487241f04034dd37f4155fba6219ca1c464e81ab
dwuu10 <121773098+dwuu10@users.noreply.github.com>
2023-10-02 11:22:39 -0700

    Initialize WeeklyChallenge6

---
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet03
+    {
+        public bool ArrayContainsAFalse(bool[] vals)
+        {
+            for (int i = vals.Length - 1; i >= 0; i--)
+            {
+                if (vals[i] == false)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
+        {
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+
+            var sum = numbers.Sum();
+
+            return (sum % 2 != 0);
+        }
+
+        public bool PasswordContainsUpperLowerAndNumber(string password)
+        {
+            var isLower = false;
+            var isUpper = false;
+            var isNumber = false;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsLower(password[i]))
+                {
+                    isLower = true;
+                }
+                if (char.IsUpper(password[i]))
+                {
+                    isUpper = true;
+                }
+                if (char.IsNumber(password[i]))
+                {
+                    isNumber = true;
+                }
+            }
+            return isLower && isUpper && isNumber;
+        }
+
+        public char GetFirstLetterOfString(string val)
+        {
+            return val[0];
+        }
+
+        public char GetLastLetterOfString(string val)
+        {
+            return val[val.Length - 1];
+        }
+
+        public decimal Divide(decimal dividend, decimal divisor)
+        {
+            if (dividend == 0 || divisor == 0)
+            {
+                return 0;
+            }
+            return dividend/divisor;
+        }
+
+        public int LastMinusFirst(int[] nums)
+        {
+            return nums[nums.Length - 1] - nums[0];
+        }
+
+        public int[] GetOddsBelow100()
+        {
+            var odds = new List<int>();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    odds.Add(i);
+                }
+            }
+            return odds.ToArray();
+        }
+
+        public void ChangeAllElementsToUppercase(string[] words)
+        {
+            for (int i = 0;i < words.Length;i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
+        }
+    }
+}
---
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet04
+    {
+        public int AddEvenSubtractOdd(int[] numbers)
+        {
+            var Evens = numbers.Where(i => i % 2 == 0);
+            var Odds = numbers.Where(i => i % 2 != 0);
+            return Evens.Sum(i => i) - Odds.Sum(i => i);
+        }
+
+        public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
+        {
+            var stringList = new List<int> { str1.Length, str2.Length, str3.Length, str4.Length };
+            return stringList.Min();
+        }
+
+        public int GetSmallestNumber(int number1, int number2, int number3, int number4)
+        {
+            var num = new List<int> { number1, number2, number3, number4 };
+            return num.Min();
+        }
+
+        public void ChangeBusinessNameTo_TrueCoders(Business biz)
+        {
+            biz.Name = "TrueCoders";
+        }
+
+        public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
+        {
+            return (
+                sideLength1 + sideLength2 > sideLength3 &&
+                sideLength1 + sideLength3 > sideLength2 &&
+                sideLength2 + sideLength3 > sideLength1
+
+                );
+        }
+
+        public bool IsStringANumber(string input)
+        {
+            var isNum = double.TryParse(input, out var num);
+            return isNum;
+        }
+
+        public bool MajorityOfElementsInArrayAreNull(object[] objs)
+        {
+            var nulls = objs.Where(o => o == null);
+            var notNull = objs.Where(o => o != null);
+            return nulls.Count() > notNull.Count();
+        }
+
+        public double AverageEvens(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            var Evens = numbers.Where(i => i % 2 != 1);
+
+            if (Evens.Count() == 0)
+            {
+                return 0;
+            }
+            return Evens.Average();
+        }
+
+        public int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+    }
+}
